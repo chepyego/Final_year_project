6673d42859d35835f0d8a1d64909122a23e1d514
chepyego <jchepyego@gmail.com>
2020-07-24 22:28:11 +0300

    added gem for uploading image

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get "/dasgboard" =>'dasgboard#index', as: :dasgboard
   get 'dasgboard/properties'
   get 'dasgboard/reports'
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
   resources :properties
   devise_for :accounts
   root to: 'page#main'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
